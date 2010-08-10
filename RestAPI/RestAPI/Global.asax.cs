12c92e3d5e5f46002cc203d4f1617a247ac1850d
nikitaknyazev <nikitaknyazev@40e57a83-7501-4b0a-8fdf-d078260c7e35>
2010-08-10 10:02:14 +0000

    git-svn-id: http://178.79.136.96/svn@302 40e57a83-7501-4b0a-8fdf-d078260c7e35

--- RestAPI/RestAPI/Global.asax.cs
+++ RestAPI/RestAPI/Global.asax.cs
@@ -24,7 +24,7 @@ namespace RestAPI
             routes.MapRoute(
                 "Default", // Route name
                 "{controller}/{action}/{id}", // URL with parameters
-                new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+                new { controller = "API", action = "Index", id = UrlParameter.Optional } // Parameter defaults
             );
             routes.MapRoute("UsersConsumption", "{controller}/users/{id}/consumption", new { controller = "API", action="UsersConsumption", id = UrlParameter.Optional });
             routes.MapRoute("UsersEvents", "{controller}/users/{id}/events", new { controller = "API", action = "UsersEvents", id = UrlParameter.Optional });

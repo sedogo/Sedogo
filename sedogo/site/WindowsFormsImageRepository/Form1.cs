f7e615ff8ffd8c4756d618ff4a6fa95b74beb3e2
mikhailtuchkov <mikhailtuchkov@40e57a83-7501-4b0a-8fdf-d078260c7e35>
2011-01-28 11:43:46 +0000

    New simple Windows project to delete all thumbnales in image repository.
    git-svn-id: http://178.79.136.96/svn@499 40e57a83-7501-4b0a-8fdf-d078260c7e35

---
+++ sedogo/site/WindowsFormsImageRepository/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsImageRepository
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {folderBrowserDialog1.ShowDialog();
+         DeleteFiles(folderBrowserDialog1.SelectedPath,false);  
+            }
+        void DeleteFiles(string path, bool deleteFiles)
+        {
+            foreach (string dpath in Directory.GetDirectories(path)) DeleteFiles(dpath,true);
+            if (!deleteFiles) return;
+            foreach (string fpath in Directory.GetFiles(path))
+            {
+                File.Delete(fpath);                
+            }
+
+        }
+    }
+}
